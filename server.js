a019ab8a5d9bdc6c519754d701727a6e5055569c
Elmir Huseynov <elmirr.huseynovv@gmail.com>
2025-11-11 17:27:01 +0400

    deploy json-server

--- server.js
+++
@@ -1,8 +0,0 @@
-import jsonServer from "json-server";
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-server.use(router);
-server.listen(3000, () => console.log("✅ JSON Server is running"));
